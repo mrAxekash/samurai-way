0cd6ffcb39681353054b77526d6ed3416f8f68a8
Aliaksandr 'Axekash' Kuchutkov <kashim007@mail.ru>
2022-09-10 22:27:44 +0300

    Change README.md

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-     Hello, samurai! Let's go!
+function App() { /*функция-компонента*/
+    return (
+    <div className="App"> /*this is JSX разметка*/
+        <ul>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>JS</li>
+            <li>React</li>
+        </ul>
     </div>
-  );
+)
+    ;
 }
 
 export default App;
