0cc851ab475b5a197680b670ceca77cda8df8bce
Aliaksandr 'Axekash' Kuchutkov <kashim007@mail.ru>
2022-09-10 22:55:12 +0300

    finish lesson 7

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,39 @@
 import React from 'react';
 import './App.css';
 
-function App() { /*функция-компонента*/
+function App() { /*функция-компонента, которая возвращает разметку HTML. Компонента не вызывается через ()*/
     return (
-    <div className="App"> /*this is JSX разметка*/
-        <ul>
-            <li>HTML</li>
-            <li>CSS</li>
-            <li>JS</li>
-            <li>React</li>
-        </ul>
-    </div>
-)
-    ;
+        <div>
+            <Header/>
+            <Technologies/>
+        </div>
+
+    )
+        ;
 }
+
+function Header() {
+    return (
+        <div>
+            <a href="#">Home</a>
+            <a href="#">News Feed</a>
+            <a href="#">Messages</a>
+        </div>
+    )
+}
+
+function Technologies() {
+    return (
+        <div className="App">
+            <ul>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>JS</li>
+                <li>React</li>
+            </ul>
+        </div>
+    )
+}
+
 
 export default App;
