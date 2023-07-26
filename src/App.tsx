ddd71fd0a13be3f92af52aca8cac89f88c1c3452
mrAxekash <aliaxandr_ku@mail.ru>
2023-07-26 21:36:38 +0300

    finish lesson 95 deploy gh-pages 1.01

--- src/App.tsx
+++ src/App.tsx
@@ -79,7 +79,7 @@ const AppContainer = compose(
 
 export const SamuraiJSApp = () => {
     return (
-        <HashRouter>
+        <HashRouter basename={process.env.PUBLIC_URL}>
             <Provider store={store}>
                 <AppContainer/>
             </Provider>
